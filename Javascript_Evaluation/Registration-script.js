c838407fde1fa51ed3c62fe6f5a7435512d2dbce
Viral Chauhan <cviral32@gmail.com>
2021-02-06 13:31:19 +0530

    seecond commit

--- Javascript_Evaluation/Registration-script.js
+++ Javascript_Evaluation/Registration-script.js
@@ -1,50 +1,65 @@
-var admin=[];
-var adminRegistered = false;
-var AdminPerson = {
-	name: tempname,
-	email: tempemail,
-	password : temppassword,
-	city : tempcity,
-	state: tempstate
-};
-document.querySelector("#registerAdmin").addEventListener('click', function(){
-	var check = document.getElementByName("terms");
-	if(check.checked){
 
-		var tempname = document.getElementByName("adminName").value;
-		var tempemail = document.getElementByName("adminEmail").value;
-		var temppassword = document.getElementByName("adminPassword").value;
-		var tempRepassword = document.getElementByName("adminRepassword").value;;
 
-		var tempstate = document.getElementByName("State").value;
-		var tempcity = document.getElementByName("City").value;
-		if(!empty(admin)){
-			for(var i =0;i<admin.length;i++){
-				if(admin.name === tempname){
-					if(admin.email === temp_email){
+
+document.querySelector("#registerAdmin").addEventListener('click', ()=>{
+	let check = document.querySelector("#terms:checked");
+	let adminRegistered = false;
+	let tempname ="";
+	let tempemail="";
+	let temppassword="";
+	let tempRepassword ="";
+	let tempstate = "";
+	let tempcity = "";
+
+	let admin= new Array();
+
+	let AdminPerson = {
+		name: "",
+		email: "",
+		password : "",
+		city : "",
+		state: ""
+	};
+	
+
+	if(check != null){
+
+		tempname = document.querySelector("#adminName").value;
+		tempemail = document.querySelector("#adminEmail").value;
+		temppassword = document.querySelector("#adminPassword").value;
+		tempRepassword = document.querySelector("#adminRepassword").value;;
+
+		tempstate = document.querySelector("#State").value;
+		tempcity = document.querySelector("#City").value;
+
+		if(admin != null){
+			for(let i =0;i<admin.length;i++){
+				if(admin.name == tempname){
+					if(admin.email == temp_email){
 						adminRegistered = true;
 					}
 				}
 			}
 		}
 		if(adminRegistered == false){
-			if(temppassword === tempRepassword){
+			if(temppassword == tempRepassword){
 				AdminPerson.name = tempname;
 				AdminPerson.email = tempemail;
 				AdminPerson.password = temppassword;
 				AdminPerson.city = tempcity;
 				AdminPerson.state = tempstate;
 			};
-			admin.push(AdminPerson);
-
-				localStorage.setItem("AdminData",admin);
+			console.log(AdminPerson);
+			
 			}
 			else{
 				alert("Password does not match");
 			}
-		}		
-	}
+	}	
+
 	else{
 		alert("Terms and Conditions not accepted");
 	}
+	admin.push(AdminPerson);
+	localStorage.setItem("AdminData",admin);
 });
